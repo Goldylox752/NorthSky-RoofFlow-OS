c52c36aa8a82530a8455412f7aabd3ca9078bf49
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-04-24 20:47:46 -0600

    Update route.js

--- app/api/stripe/create-checkout-session/route.js
+++ app/api/stripe/create-checkout-session/route.js
@@ -1,8 +1,14 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
 export async function POST(req) {
+  const key = process.env.STRIPE_SECRET_KEY;
+
+  if (!key) {
+    throw new Error("Missing STRIPE_SECRET_KEY");
+  }
+
+  const stripe = new Stripe(key);
+
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",

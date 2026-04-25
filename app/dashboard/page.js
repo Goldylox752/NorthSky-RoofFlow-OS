a6ccb2b77c9e36c9ac71477bcab3387e596742d3
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-04-25 09:41:26 -0600

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,7 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
-import { useRouter } from "next/navigation";
+export default async function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
